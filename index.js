32b76eea9b545892fc8dfb25997fa69ba516247c
shayaanakbar <shayaanakbar1106@gmail.com>
2019-05-08 15:53:17 -0500

    Server is responding

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // implement your API here
+// Add Express
+const express = require('express');
+const db = require('./data/db.js')
+
+//Create express application using the express module
+
+const server = express();
+server.use(express.json())
+
+server.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+
+server.listen(9090, () =>{
+  console.log('listening on port 9090');
+});
