b803e320ab8f1b9d082cdc56fe4efe9aff70759f
shayaanakbar <shayaanakbar1106@gmail.com>
2019-05-08 16:01:20 -0500

    Added get + Post

--- index.js
+++ index.js
@@ -8,8 +8,27 @@ const db = require('./data/db.js')
 const server = express();
 server.use(express.json())
 
-server.get('/', (req, res) => {
-  res.send('Hello World');
+server.get('/users', (req, res) => {
+  db.find()
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(error =>{
+      res.status(500).json({error: err, message: 'whoops!'})
+  })
+})
+
+server.post('/api/users', (req, res) => {
+  const newUser = req.body
+  console.log('req.body', req.body);
+  db.add(newUser)
+    .then(addedUser => {
+      // 201 status is added when new user
+      res.status(201).json(addedUser)
+      })
+    .catch(({code, message}) => {
+    res.status(code).json({ err: message });
+  });
 });
 
 
